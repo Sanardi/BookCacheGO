1a328dca7ead63ff8a508cc3c86bbf855651868b
sanardiazam <rubypromise@gmx.com>
2021-06-06 12:28:03 +0100

    channels working as expected

--- main.go
+++ main.go
@@ -13,23 +13,40 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
 	for i := 0; i < 8; i++ {
 		id := rnd.Intn(3) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryCache(id, m); ok {
+				//fmt.Println("from cache")
+				//fmt.Println(b)
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, cacheCh)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
+			if b, ok := queryDatabase(id, m); ok {
+				//fmt.Println("from Database")
+				//fmt.Println(b)
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
+
+		//create one goroutine per query to handle the response
+		go func(cacheCn, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
 				fmt.Println("from cache")
 				fmt.Println(b)
-			}
-			wg.Done()
-		}(id, wg, m)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
-			if b, ok := queryDatabase(id, m); ok {
-				fmt.Println("from Database")
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("from database")
 				fmt.Println(b)
 			}
-			wg.Done()
-		}(id, wg, m)
+		}(cacheCh, dbCh)
 		time.Sleep(150 * time.Millisecond)
 	}
 
